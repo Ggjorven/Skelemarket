c7fded06ce6d6151d40b556b358836bfdeb51fe5
ggjorven <jorbenvanderwal@gmail.com>
2026-09-02 10:51:58 +0200

    Initial files.

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    id("java")
+    id("application")
+    id("eclipse") // Forces JDT-compatible classpath metadata
+}
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+group = "skelemarket"
+version = "0.1-alpha"
+
+repositories {
+    mavenCentral()
+}
+
+sourceSets {
+    main {
+        java.setSrcDirs(listOf("src"))
+    }
+    test {
+        java.setSrcDirs(listOf("tests"))
+    }
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:6.0.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+application {
+    mainClass.set("skelemarket.Main")
+}
+
+tasks.jar {
+    manifest {
+        attributes["Main-Class"] = "skelemarket.Main"
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
