54cf9381d7b7d5720ec7b506adc76c15781f9a90
ggjorven <jorbenvanderwal@gmail.com>
2026-09-03 10:27:40 +0200

    Add first JavaFX source code.

--- src/skelemarket/Main.java
+++ src/skelemarket/Main.java
@@ -1,7 +1,15 @@
 package skelemarket;
 
-public class Main {
-	public static void main(String[] args) {
-        System.out.println("Hello, world!");
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        System.out.println("Hello, world");
     }
 }
