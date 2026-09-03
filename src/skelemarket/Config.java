fee28a55a76a60c5278920e1cfa3c09cd73bf01d
ggjorven <jorbenvanderwal@gmail.com>
2026-09-03 19:07:18 +0200

    Add first nicely seperated Main/Simulation and Config files.

---
+++ src/skelemarket/Config.java
@@ -0,0 +1,7 @@
+package skelemarket;
+
+public class Config {
+	public static final int WIDTH = 800;
+	public static final int HEIGHT = 600;
+	public static final String TITLE = "Skelemarket simulation";
+}
---
+++ src/skelemarket/Simulation.java
@@ -0,0 +1,51 @@
+package skelemarket;
+
+import javafx.animation.AnimationTimer;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class Simulation {
+	///////////////////////////////////////////////////////////
+	// Variables
+	///////////////////////////////////////////////////////////
+	private Canvas mCanvas = null;
+	private GraphicsContext mContext = null;
+
+	private Pane mPane = null;
+	private Scene mScene = null;
+
+	///////////////////////////////////////////////////////////
+	// Methods
+	///////////////////////////////////////////////////////////
+	public Simulation(Stage stage) {
+        mCanvas = new Canvas(Config.WIDTH, Config.HEIGHT);
+        mContext = mCanvas.getGraphicsContext2D();
+
+        mPane = new Pane(mCanvas);
+		mScene = new Scene(mPane);
+
+        stage.setScene(mScene);
+        stage.setTitle(Config.TITLE);
+        stage.show();
+
+		// TODO: Custom renderer
+	}
+
+	public void run() {
+		new AnimationTimer() {
+            private long previousTime = System.nanoTime();
+
+			@Override
+			public void handle(long now) {
+				double deltaTime = (now - previousTime) / 1_000_000_000.0f;
+				previousTime = now;
+
+				// TODO: Update
+				// TODO: Render
+			}
+		}.start();
+	}
+}
