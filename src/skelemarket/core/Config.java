d087604bc087e6dee5a133399f3695f68693bde6
ggjorven <jorbenvanderwal@gmail.com>
2026-09-07 12:00:18 +0200

    Fix width and height.

--- src/skelemarket/core/Config.java
+++ src/skelemarket/core/Config.java
@@ -1,7 +1,7 @@
 package skelemarket.core;
 
 public class Config {
-	public static final int WIDTH = 1280;
-	public static final int HEIGHT = 720;
+	public static final int WIDTH = 1280; // 20 tiles
+	public static final int HEIGHT = 738; // 13 tiles
 	public static final String TITLE = "Skelemarket simulation";
 }
