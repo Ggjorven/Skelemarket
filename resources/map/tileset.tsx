c2fbe3e0e9caa677bafb14f747bc3954740fdd01
ggjorven <jorbenvanderwal@gmail.com>
2026-09-07 11:59:58 +0200

    Move resources to subfolder.

---
+++ resources/map/tileset.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="tileset" tilewidth="640" tileheight="512" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image source="../textures/pumpkin/sheet.png" width="512" height="512"/>
+ </tile>
+ <tile id="1">
+  <image source="../textures/dungeon/walls_floor.png" width="272" height="464"/>
+ </tile>
+ <tile id="2">
+  <image source="../textures/carbonova-supermarket/tiles.png" width="112" height="160"/>
+ </tile>
+ <tile id="3">
+  <image source="../textures/cozy-groceries/sheet.png" width="208" height="304"/>
+ </tile>
+ <tile id="4">
+  <image source="../textures/admurins-halloween/sheet.png" width="640" height="32"/>
+ </tile>
+</tileset>
--- resources/pumpkin/README.md
+++
@@ -1,3 +0,0 @@
-# Pumpkin
-
-These assets are from: [https://www.cleanpng.com/png-tile-based-video-game-house-plan-furniture-2d-furn-llcnpp/](https://www.cleanpng.com/png-tile-based-video-game-house-plan-furniture-2d-furn-llcnpp/)
