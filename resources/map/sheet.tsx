2b9a05c96969fcf6e53df19d9c229dc121f1b5bc
stephanwiard742 <stephanwiard742@gmail.com>
2026-09-08 12:43:55 +0200

    First changes to assets (deleted old and added new ones).
    Deleted all previous assets and added new assets with right dimentions
    (the basic assets for walls etc)

---
+++ resources/map/sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.2" name="sheet" tilewidth="32" tileheight="32" tilecount="54" columns="6">
+ <image source="../textures/cozy-groceries/sheet.png" width="208" height="304"/>
+</tileset>
