3e7830ed88172b766a7e48b3177d421dccfca656
ggjorven <jorbenvanderwal@gmail.com>
2026-09-08 12:54:33 +0200

    Attempt to fix relative files.

--- resources/map/decorative_cracks_floor.tsx
+++ resources/map/decorative_cracks_floor.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="decorative_cracks_floor" tilewidth="32" tileheight="32" tilecount="120" columns="8">
- <image source="../../../Downloads/decorative_cracks_floor.png" width="256" height="480"/>
+ <image source="../textures/enhanced/decorative_cracks_floor.png" width="256" height="480"/>
 </tileset>
--- resources/map/doors_lever_chest_animation.tsx
+++ resources/map/doors_lever_chest_animation.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.12.2" name="doors_lever_chest_animation" tilewidth="32" tileheight="32" tilecount="150" columns="10">
- <image source="../../../Downloads/doors_lever_chest_animation.png" width="320" height="480"/>
+ <image source="../textures/enhanced/doors_lever_chest_animation.png" width="320" height="480"/>
 </tileset>
