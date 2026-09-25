feb7c3a542c365bd72b2e66c8ca8dcbb739b5953
ggjorven <jorbenvanderwal@gmail.com>
2026-09-25 15:37:36 +0200

    Small fixes.

--- src/skelemarket/simulation/Supermarket.java
+++ src/skelemarket/simulation/Supermarket.java
@@ -1,13 +1,13 @@
 package skelemarket.simulation;
 
+import skelemarket.core.Logger;
 import skelemarket.core.Renderer;
 
 public class Supermarket {
 	///////////////////////////////////////////////////////////
 	// Variables
 	///////////////////////////////////////////////////////////
-	
-	
+
 	///////////////////////////////////////////////////////////
 	// Methods
 	///////////////////////////////////////////////////////////
@@ -15,28 +15,27 @@ public class Supermarket {
 		// Create/load map
 
 		// Spawn customers at the door
-		
 
 		// Spawn manager
-		
+
 		// Spawn cashier
-		
 
 		// Spawn 3 random stockers
 	}
 
 	public void update() {
+		Logger.trace("test");
+
 		// Update all customers
-		
+
 		// Update all employees
 	}
 
 	public void render(Renderer rendererRef) {
 		// Render map
-		
+
 		// Render customers
-		
-		
+
 		// Render employees
 	}
 }
